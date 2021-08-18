34eea329af555d03eeefa4cfc82479afef5fd0e3
Kevin <kevin.gonzalezr@hotmail.es>
2021-08-18 13:10:16 -0400

    finished lesson 11 state and getters

--- README.md
+++ README.md
@@ -1,24 +1,35 @@
 # real-world-vue
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
+### Serve mock database for development
+
+```
+json-server --watch db.json
+```
+
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>{{ getEventById(1) }}</p>
+    <ul>
+      <li v-for="category in categories" :key="category">{{ category }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+export default {
+  computed: {
+    getEvent() {
+      return this.$store.getters.getEventById
+    },
+    ...mapState(['user', 'categories']),
+    ...mapGetters(['getEventById']),
+  },
+  /*{ named state
+    //userName: (state) => state.user.name, dot notation
+    user: 'user', //string notation
+    categories: 'categories',
+    }*/
+}
+</script>
+
+<style scoped></style>
